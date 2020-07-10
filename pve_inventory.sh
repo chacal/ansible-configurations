37b881158a096875ce66a81dd13e4d1de5b4a77d
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2020-07-10 15:01:05 +0300

    Fix inventory creation script
    Earlier version added an extra "," at the end of the last item in the list

--- pve_inventory.sh
+++ pve_inventory.sh
@@ -1,17 +1,12 @@
 #!/usr/bin/env bash
-LIST_CMD="qm list | tail -n+2 | grep running | grep -v pfsense | awk '{print \$2}'"
-WARIO_VMS=$(ssh wario.chacal.fi "sudo ${LIST_CMD}")
-FUJARI_VMS=$(ssh fujari.chacal.fi "${LIST_CMD}")
+LIST_CMD='qm list | tail -n+2 | grep running | grep -v pfsense | awk "{print \"\42\" \$2 \"\42\"}" | paste -d, -s'
+WARIO_VMS=$(ssh wario.chacal.fi sudo ${LIST_CMD})
+FUJARI_VMS=$(ssh fujari.chacal.fi ${LIST_CMD})
 
 echo "{"
 echo "  \"all\": {"
 echo "    \"hosts\": ["
-while IFS= read -r line; do
-    echo "      \"$line\","
-done <<< "${WARIO_VMS}"
-while IFS= read -r line; do
-    echo "      \"$line\","
-done <<< "${FUJARI_VMS}"
+echo $WARIO_VMS,$FUJARI_VMS
 echo "    ]"
 echo "  }"
 echo "}"
