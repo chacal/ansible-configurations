74b2043089b9792a308ce3d862f97cfaeb80fb27
Jouni Hartikainen <jouni.hartikainen@reaktor.com>
2023-03-31 08:40:07 +0300

    Wario is no more, use Hessu instead

--- pve_inventory.sh
+++ pve_inventory.sh
@@ -1,12 +1,12 @@
 #!/usr/bin/env bash
-LIST_CMD='qm list | tail -n+2 | grep running | grep -v pfsense | awk "{print \"\42\" \$2 \"\42\"}" | paste -d, -s'
-WARIO_VMS=$(ssh wario.chacal.fi sudo ${LIST_CMD})
+LIST_CMD='qm list | tail -n+2 | grep running | grep -v pfsense | grep -v wg-endpoint | awk "{print \"\42\" \$2 \"\42\"}" | paste -d, -s'
+HESSU_VMS=$(ssh hessu.chacal.fi sudo ${LIST_CMD})
 FUJARI_VMS=$(ssh fujari.chacal.fi ${LIST_CMD})
 
 echo "{"
 echo "  \"all\": {"
 echo "    \"hosts\": ["
-echo $WARIO_VMS,$FUJARI_VMS
+echo $HESSU_VMS,$FUJARI_VMS
 echo "    ]"
 echo "  }"
 echo "}"
